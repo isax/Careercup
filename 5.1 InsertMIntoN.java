3f03f2a1292f06234df7f23e0e7514712b531ae4
isax <chengyan.isa@gmail.com>
2014-01-26 10:46:47 -0500

    Create 5.1 InsertMIntoN.java

---
+++ 5.1 InsertMIntoN.java
@@ -0,0 +1,16 @@
+public class InsertMIntoN {
+	
+	public static int insert(int M, int N, int i, int j){
+		int allOnes = ~0;
+		int s = (~allOnes)<<j+1;
+		
+		int t = (1<<i)-1;
+		
+		int mask = s|t;
+		
+		int n_cleared = N & mask;
+		int m_shifted = M<<i;	
+		
+		return n_cleared | m_shifted;	
+	}
+}
