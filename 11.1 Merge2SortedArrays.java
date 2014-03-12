bfa5fb1a0834eccef5c6dec96ca4e785daab9d01
isax <isax@users.noreply.github.com>
2014-03-11 23:36:07 -0400

    Update 11.1 Merge2SortedArrays.java

--- 11.1 Merge2SortedArrays.java
+++ 11.1 Merge2SortedArrays.java
@@ -7,10 +7,6 @@ public class Merge2SortedArrays {
                 int lastA = m-1;
                 int lastB = n-1;
                 
-                if(m<0||m>A.length||n<0||n>B.length||indexLast>A.length-1){
-                        return;
-                }
-                        
                 while(lastA>=0 && lastB>=0){
                         if(A[lastA]>=B[lastB]) {
                                 A[indexLast]=A[lastA];
